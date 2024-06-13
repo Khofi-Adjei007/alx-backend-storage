d2bc5be036eedfec64b78b4891cd56d92739cba2
Khofi-Adjei007 <khofiadjei@gmail.com>
2024-06-13 23:46:49 +0000

    Smashing Quest II - We won

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- This script creates a table named 'users' with specified attributes.
+-- The table will only be created if it does not already exist.
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
+);
